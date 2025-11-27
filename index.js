db50381ae2b9e25aa5e08b218056be1a13be05a4
Gabriel Teles <bilebile2020@gmail.com>
2025-11-27 16:30:37 -0300

    Modifica URL de fetch

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ contactForm.addEventListener("submit", async (ev) => {
         feedbackElem.style.color = "black";
         feedbackElem.textContent = "Enviando...";
 
-        const response = await fetch("https://contact-api.gabrieltlss.com.br/send", {
+        const response = await fetch("https://contact-api.gabrieltlss.com.br/", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, email, message })
